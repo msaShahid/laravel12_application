7f375ca382dd2ee830658f9cfed55f5e1c6669e1
Mohammad Shahid <msa.ansari28@gmail.com>
2025-03-08 23:54:16 +0530

    "Added 'posts' data to Inertia::render in PostController index method"

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,9 @@ class PostController extends Controller
      */
     public function index(): Response
     {
-        return Inertia::render('Posts');
+        return Inertia::render('Posts',[
+            'posts' => Post::all(),
+        ]);
     }
 
     /**
