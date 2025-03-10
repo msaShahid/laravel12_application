1237f4c17d9d722826266bb8c90858c089e4d0c4
Mohammad Shahid <msa.ansari28@gmail.com>
2025-03-10 23:46:50 +0530

    `Refactored database migration and add more migration file`

--- database/migrations/2025_03_10_171022_create_categories_table.php
+++ database/migrations/2025_03_10_171022_create_categories_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('slug')->nullable();;
-            $table->text('content');
-            $table->string('image')->nullable();
+            $table->string('name');
+            $table->string('slug')->nullable()->unique();
+            $table->text('description')->nullable();
             $table->timestamps();
+            $table->softDeletes(); 
         });
     }
 
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('categories');
     }
 };
---
+++ database/migrations/2025_03_10_172953_create_posts_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->nullable()->unique();
+            $table->text('content');
+            $table->string('image')->nullable();
+            $table->string('excerpt')->nullable();
+            $table->boolean('is_published')->default(false);
+            $table->timestamp('published_at')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->json('tags')->nullable();
+            $table->string('meta_title')->nullable();
+            $table->string('meta_description')->nullable();
+            $table->string('meta_keywords')->nullable();
+            $table->timestamps();
+            $table->softDeletes(); 
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
