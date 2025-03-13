fd08762278a8df7b0f207884b5069c98fe98a16b
Mohammad Shahid <msa.ansari28@gmail.com>
2025-03-14 00:26:23 +0530

    `Update AuthenticatedSessionController to use admin namespace and routes`

--- app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
@@ -4,7 +4,7 @@
 namespace App\Http\Controllers\Admin\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\admin\Auth\LoginRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -34,7 +34,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended(route('admin.dashboard', absolute: false));
     }
 
     /**
@@ -42,11 +42,11 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request): RedirectResponse
     {
-        Auth::guard('web')->logout();
+        Auth::guard('admin')->logout();
 
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect('/admin');
     }
 }
